e91afc74da193bb50ad0ec265758a9d0d89735a8
mynjstr <jestertiedra@gmail.com>
2024-04-23 21:42:53 +0800

    created pseudocode

---
+++ program1/read_and_separate_even_and_odd.py
@@ -0,0 +1,12 @@
+#pseudocode
+#create numbers.txt
+#list 20 integers
+#open .txt file and separately read each line from the .txt file
+#separate even and odd numbers extracted from the file
+#if the number is even
+#open the "even.txt" file and write the even numbers extracted
+#if the number is odd
+#open the"odd.txt" file and write the odd numbers extracted
+
+        
+
