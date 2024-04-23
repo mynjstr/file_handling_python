c3c7184c230deec4af2e34eacfe9cfe80a916d5c
mynjstr <jestertiedra@gmail.com>
2024-04-23 22:02:58 +0800

    mistakes corrected

--- program1/read_and_separate_even_and_odd.py
+++ program1/read_and_separate_even_and_odd.py
@@ -3,12 +3,15 @@
 #list 20 integers
 #open .txt file and separately read each line from the .txt file
 with open("numbers.txt", "r") as integers_file:
-    for line in integers_file
-    line = line.strip()
+    for line in integers_file:
+        line = line.strip()
 #separate even and odd numbers extracted from the file
 #if the number is even
+
 #open the "even.txt" file and write the even numbers extracted
+
 #if the number is odd
+
 #open the"odd.txt" file and write the odd numbers extracted
 
         
