934578d45dec6a2ed16223157e7fb9da2ae92d7d
mynjstr <jestertiedra@gmail.com>
2024-04-23 22:52:44 +0800

    if the number is even, open the even.txt file and write the even numbers extracted

--- program1/read_and_separate_even_and_odd.py
+++ program1/read_and_separate_even_and_odd.py
@@ -5,14 +5,14 @@
 with open("numbers.txt", "r") as integers_file:
     for line in integers_file:
         line = line.strip()
+
 #separate even and odd numbers extracted from the file
 #if the number is even
+        if int(line) % 2 == 0:
 
 #open the "even.txt" file and write the even numbers extracted
+            with open("even.txt", "a") as even_file:
+                even_file.write(f'{line}\n')
 
 #if the number is odd
-
 #open the"odd.txt" file and write the odd numbers extracted
-
-        
-
