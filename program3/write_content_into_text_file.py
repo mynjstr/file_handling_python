c99ab9d5b1c876b43f63d708cc9c452b71da6f12
mynjstr <jestertiedra@gmail.com>
2024-04-25 00:50:12 +0800

    inquire the user as to whether additional lines are needed

--- program3/write_content_into_text_file.py
+++ program3/write_content_into_text_file.py
@@ -9,12 +9,14 @@ while resume:
 
 #Insert a newline character at the line's end.
     input_line_break = input_line + ("\n")
-    
+
 #Open mylife.txt file in append mode
     with open("mylife.txt", "a") as mylife_file:
         mylife_file.write(input_line)
-
 #inquire the user as to whether additional lines are needed.
+        while resume:
+                extra = input("Are there more lines y/n? ")
+                
 # Break the inner loop if the user wishes to add additional lines.
 ## When the user is finished, break both loops and print a message.
 # Produce a message indicating that the user's input is incorrect
