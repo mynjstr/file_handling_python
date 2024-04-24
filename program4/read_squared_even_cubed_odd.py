31326fe98dd13b6d65f45dc3b51af7f8fe9e11ff
mynjstr <jestertiedra@gmail.com>
2024-04-25 01:16:48 +0800

    created pseudocode

---
+++ program4/read_squared_even_cubed_odd.py
@@ -0,0 +1,11 @@
+#pseudocode
+#create integers.txt
+#list 20 integers
+#open .txt file and separately read each line from the .txt file
+#separate even and odd numbers extracted from the file
+#if the number is even
+#Put each even integer into double.txt and square them.
+#open the "even.txt" file and write the squared even numbers extracted
+#if the number is odd
+#Put each odd integer into triple.txt and cube them.
+#open the"odd.txt" file and write the cubed odd numbers extracted
