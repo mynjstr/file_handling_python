fdd68a9076a58e4072999531c7b61b9a009a65ea
mynjstr <jestertiedra@gmail.com>
2024-04-24 17:13:37 +0800

    import tkinter

--- program2/finding_highest_gwa_from_students.py
+++ program2/finding_highest_gwa_from_students.py
@@ -1,7 +1,9 @@
 #pseudocode
 #create a file with 20 student names and their gwa
 #import tkinter
+import tkinter as tk
 #output highest gwa student from the file
+
 #read students_and_gwa.txt 
 #find highest gwa student and their gwa
 #output highest gwa student from the file with their name and their gwa using tkinter
