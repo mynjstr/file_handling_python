7a4df00b5ee3bbc75a05d59bc8fceeadc85c0476
mynjstr <jestertiedra@gmail.com>
2024-04-25 01:19:37 +0800

    open .txt file and separately read each line from the .txt file

--- program4/read_squared_even_cubed_odd.py
+++ program4/read_squared_even_cubed_odd.py
@@ -2,6 +2,10 @@
 #create integers.txt
 #list 20 integers
 #open .txt file and separately read each line from the .txt file
+with open("numbers.txt", "r") as integers_file:
+    for line in integers_file:
+        line = line.strip()
+
 #separate even and odd numbers extracted from the file
 #if the number is even
 #Put each even integer into double.txt and square them.
