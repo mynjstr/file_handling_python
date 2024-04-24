405633bb647833e309ed2b36aea424a03a5b261d
mynjstr <jestertiedra@gmail.com>
2024-04-24 17:26:03 +0800

    read students_and_gwa.txt

--- program2/finding_highest_gwa_from_students.py
+++ program2/finding_highest_gwa_from_students.py
@@ -3,7 +3,13 @@
 #import tkinter
 import tkinter as tk
 #output highest gwa student from the file
-
 #read students_and_gwa.txt 
+def get_highest_gwa(directory):
+    with open(directory, "r") as students_and_gwa_file:
+        info = students_and_gwa_file.read()
+        info_record = eval(info)
+        highest_gwa = None
+        highest_gwa_student = None
 #find highest gwa student and their gwa
+
 #output highest gwa student from the file with their name and their gwa using tkinter
