d202d07195316fbbd14325d10773b5959d748809
mynjstr <jestertiedra@gmail.com>
2024-04-24 16:34:40 +0800

    created pseudocode

---
+++ program2/finding_highest_gwa_from_students.py
@@ -0,0 +1,7 @@
+#pseudocode
+#create a file with 20 student names and their gwa
+#import tkinter
+#output highest gwa student from the file
+#read students_and_gwa.txt 
+#find highest gwa student and their gwa
+#output highest gwa student from the file with their name and their gwa using tkinter
