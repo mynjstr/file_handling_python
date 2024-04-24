d06234be3b80f6d50ae8e8c30d16c52b7908e85e
mynjstr <jestertiedra@gmail.com>
2024-04-25 01:31:13 +0800

    if the number is odd,put each odd integer into triple.txt and cube them

--- program4/read_squared_even_cubed_odd.py
+++ program4/read_squared_even_cubed_odd.py
@@ -12,11 +12,14 @@ with open("integers.txt", "r") as integers_file:
         if int(line) % 2 == 0:
 #Put each even integer into double.txt and square them.
             squared_even_numbers = line ** 2
-            
-#open the "even.txt" file and write the squared even numbers extracted
+
+#open the "double.txt" file and write the squared even numbers extracted
             with open("double.txt", "a") as squared_even_file:
                 squared_even_file.write(f'{squared_even_numbers}\n')
 
 #if the number is odd
 #Put each odd integer into triple.txt and cube them.
-#open the"odd.txt" file and write the cubed odd numbers extracted
+        else:
+            cubed_odd_numbers = line ** 3
+            
+#open the"triple.txt" file and write the cubed odd numbers extracted
