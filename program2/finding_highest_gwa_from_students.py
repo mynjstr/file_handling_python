b955d8dca0ac173e16696fff97858584847f640a
mynjstr <jestertiedra@gmail.com>
2024-04-24 17:39:54 +0800

    find highest gwa student and their gwa

--- program2/finding_highest_gwa_from_students.py
+++ program2/finding_highest_gwa_from_students.py
@@ -10,6 +10,16 @@ def get_highest_gwa(directory):
         info_record = eval(info)
         highest_gwa = None
         highest_gwa_student = None
+
 #find highest gwa student and their gwa
 
+        for student, grade in info_record.items():
+            float_grade = float(grade)
+            if highest_gwa is None or float_grade < float(highest_gwa):
+                highest_gwa_student = student
+                highest_gwa = grade
+        return highest_gwa_student, highest_gwa
+
 #output highest gwa student from the file with their name and their gwa using tkinter
+
+    
