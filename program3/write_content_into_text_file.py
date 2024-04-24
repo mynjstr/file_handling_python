cd60747f8f7b3e46d3b959f630db6b75386f2be0
mynjstr <jestertiedra@gmail.com>
2024-04-24 23:58:15 +0800

    created pseudocode

---
+++ program3/write_content_into_text_file.py
@@ -0,0 +1,10 @@
+#pseudocode
+#write many lines of content into a text file named mylife.txt.
+#continue requesting additional lines until the person rejects.
+#Ask for the user's input
+#Insert a newline character at the line's end.
+#Open mylife.txt file in append mode
+#inquire the user as to whether additional lines are needed.
+# Break the inner loop if the user wishes to add additional lines.
+## When the user is finished, break both loops and print a message.
+# Produce a message indicating that the user's input is incorrect
