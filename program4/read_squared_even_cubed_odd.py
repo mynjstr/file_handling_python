f5fe6174ed12e7e19cb7fb183d579aea8e1f1682
mynjstr <jestertiedra@gmail.com>
2024-04-25 01:28:34 +0800

    pseucode adjustment wrong file names

--- program4/read_squared_even_cubed_odd.py
+++ program4/read_squared_even_cubed_odd.py
@@ -12,10 +12,11 @@ with open("integers.txt", "r") as integers_file:
         if int(line) % 2 == 0:
 #Put each even integer into double.txt and square them.
             squared_even_numbers = line ** 2
+            
+#open the "even.txt" file and write the squared even numbers extracted
             with open("double.txt", "a") as squared_even_file:
                 squared_even_file.write(f'{squared_even_numbers}\n')
 
-#open the "even.txt" file and write the squared even numbers extracted
 #if the number is odd
 #Put each odd integer into triple.txt and cube them.
 #open the"odd.txt" file and write the cubed odd numbers extracted
