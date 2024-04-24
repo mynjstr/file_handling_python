741d5a0c2c707d8ea17baac5173db2169296ad39
mynjstr <jestertiedra@gmail.com>
2024-04-25 00:45:11 +0800

    Open mylife.txt file in append mode and write the line to the file

--- program3/write_content_into_text_file.py
+++ program3/write_content_into_text_file.py
@@ -6,11 +6,14 @@ while resume:
 
 #Ask for the user's input
     input_line = ("Enter line: ")
-    
+
 #Insert a newline character at the line's end.
     input_line_break = input_line + ("\n")
-
+    
 #Open mylife.txt file in append mode
+    with open("mylife.txt", "a") as mylife_file:
+        mylife_file.write(input_line)
+
 #inquire the user as to whether additional lines are needed.
 # Break the inner loop if the user wishes to add additional lines.
 ## When the user is finished, break both loops and print a message.
