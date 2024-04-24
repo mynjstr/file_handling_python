95c9f0dccd04c4264278f1b54f10af6e2773c306
mynjstr <jestertiedra@gmail.com>
2024-04-25 00:52:08 +0800

    Break the inner loop if the user wishes to add additional lines

--- program3/write_content_into_text_file.py
+++ program3/write_content_into_text_file.py
@@ -16,7 +16,9 @@ while resume:
 #inquire the user as to whether additional lines are needed.
         while resume:
                 extra = input("Are there more lines y/n? ")
-                
 # Break the inner loop if the user wishes to add additional lines.
+                if extra.lower() == "y":
+                    break
+                
 ## When the user is finished, break both loops and print a message.
 # Produce a message indicating that the user's input is incorrect
